98fc4774a1d2daa791f64321ea1ce0208133bb1e
ChungYeul In <sldk86@naver.com>
2014-01-06 14:08:03 +0900

    p140 실습하였습니다.
    p140 실습하였습니다.

--- CountDownPickerViewTest/ViewController.m
+++ CountDownPickerViewTest/ViewController.m
@@ -9,10 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+- (void)stopTimer;
+- (void)startTimer;
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSTimer *timer;
+    BOOL timerWorking;
+}
+// 각초마다 1분씩 감소
+- (void)tickDown:(NSTimer *)timer {
+    self.datePicker.countDownDuration = self.datePicker.countDownDuration - 60;
+    // 1분 이내면 정지
+    if (self.datePicker.countDownDuration <= 60) {
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+// 타이머 정지
+- (void)stopTimer {
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+
+// 타이머 시작
+- (void)startTimer {
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
+}
+
+- (IBAction)toggleTimer:(id)sender {
+    // 버튼 토글 설정
+    NSString *title = timerWorking ? @"Start" : @"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    
+    if (timerWorking) {
+        [self startTimer];
+    }
+    else {
+        [self stopTimer];
+    }
+}
 
 - (void)viewDidLoad
 {
